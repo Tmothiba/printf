f4f32549b59db92cb67a7f3a73d7ec9dbeac48a8
Tmothiba <mothibatshwarelo99@gmail.ccom>
2023-09-20 14:08:40 -0700

    get_precision.c

---
+++ get_precision.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * get_precision - this function calculates the precision for printing formats
+ * @format: pointer to a formatted string.
+ * @i: lists of arguments
+ * @list: list of arguments
+ * Return: int
+ */
+int get_precision(const char *format, int *i, va_list list)
+{
+	int cur = *i + 1, precision = -1;
+
+	if (format[cur] != '.')
+	{
+		return (precision);
+	}
+	precision = 0;
+	for (cur = 1; fromat[cur] != '\0'; cur++)
+	{
+		if (is_digit(format[cur]))
+		{
+			precision *= 10;
+			precision += format[cur] - '0';
+		}
+		else if (format[cur] == '*')
+		{
+			cur++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+		{
+		break;
+		}
+	}
+	*i = cur - 1;
+	return (precision);
+}
+
+
+
+
